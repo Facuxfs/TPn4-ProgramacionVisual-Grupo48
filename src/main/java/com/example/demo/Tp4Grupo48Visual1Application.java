74994d650cd0d822035995d6f0f3dddb703b993e
Facundo Romero <33964309+Facuxfs@users.noreply.github.com>
2022-04-26 21:53:50 -0300

    Trabajo Practico n4 completo

---
+++ src/main/java/com/example/demo/Tp4Grupo48Visual1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp4Grupo48Visual1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp4Grupo48Visual1Application.class, args);
+	}
+
+}
